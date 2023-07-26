d2f19f5d9f9cd856b02b381240c5ab2f3c5df912
kauannlima <124539523+kauannlima@users.noreply.github.com>
2023-07-26 00:42:15 -0300

    Atualizações na quebra de linha do codigo

--- js/carousel.js
+++ js/carousel.js
@@ -1,53 +1,53 @@
-function Carousel(config){
-    this.container = ( typeof config.container === 'string') ? document.querySelector(config.container) : config.container
-    
+function Carousel(config) {
+    this.container = (typeof config.container === 'string') ? document.querySelector(config.container) : config.container
+
     this.itens = (typeof config.itens === 'string') ? this.container.querySelectorAll(config.itens) : config.itens
-    
+
     this.btnPrev = (typeof config.btnPrev === 'string') ? this.container.querySelector(config.btnPrev) : config.btnPrev
-    
+
     this.btnNext = (typeof config.btnNext === 'string') ? this.container.querySelector(config.btnNext) : config.btnNext
-    
+
     var _this = this;
     var _currentSlide = 0
-    
+
     init()
-    
-    function init(){
+
+    function init() {
         var _show = _this.container.querySelectorAll('.show')
-        
-        Array.prototype.forEach.call(_show, function(sh){
+
+        Array.prototype.forEach.call(_show, function (sh) {
             sh.classList.remove('show')
         })
         _this.itens[0].classList.add('show')
         _this.btnNext.removeAttribute('style')
         _this.btnPrev.removeAttribute('style')
-        
-        addListeners()        
+
+        addListeners()
     }
-    
-    function addListeners(){
+
+    function addListeners() {
         _this.btnNext.addEventListener('click', showNextSlide)
         _this.btnPrev.addEventListener('click', showPrevSlide)
     }
-    
-    function showNextSlide(){
+
+    function showNextSlide() {
         _currentSlide++;
         showSlide()
     }
-    
-    function showPrevSlide(){
+
+    function showPrevSlide() {
         _currentSlide--;
         showSlide()
     }
-    
-    function showSlide(){
+
+    function showSlide() {
         var qtd = _this.itens.length;
         var slide = _currentSlide % qtd;
         slide = Math.abs(slide);
-        
+
         _this.container.querySelector('.show').classList.remove('show');
         _this.itens[slide].classList.add('show')
-        
+
     }
-    
+
 }
