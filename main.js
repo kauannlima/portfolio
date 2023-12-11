11e642a8bcca0cf8919d0f680f307696d3eee327
Kauan Lima <kauanalmeidalima1405@gmail.com>
2023-12-11 00:19:22 -0300

    Atualizações no Carousel dos certifcados e CSS

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 (function () {
-    var $body = document.querySelector("body")
-    $body.classList.remove("no-js")
-    $body.classList.add("js")
+    var $body = document.querySelector("body");
+    $body.classList.remove("no-js");
+    $body.classList.add("js");
 
-
+  
     var menu = new Menu({
         container: ".header__nav",
         toggleBtn: ".header__btnMenu",
         widthEnabled: 750
     })
-    var carouselImgs = new Carousel({
-        container: '.laptop-slider .slideshow',
-        itens: 'figure',
-        btnPrev: '.prev',
-        btnNext: '.next'
-    })
+  
+  
+   
 
     window.addEventListener('scroll', setupNav)
 
